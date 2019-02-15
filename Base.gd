7eaa5dd43423ffce0c5c4c38a7dee869074ad58d
Your Name <tobbe_chevy@hotmail.com>
2019-02-15 21:02:03 +0100

    Added changing Cursor
    Added changing cursor for watering and fertilizer

--- Base.gd
+++ Base.gd
@@ -14,6 +14,8 @@ enum MODES{
 
 export var water_cursor : Resource
 export var fertilizer_cursor : Resource 
+export var water_cursor_clicked : Resource
+export var fertilizer_cursor_clicked : Resource 
 export var hand_cursor : Resource 
 
 var potScene = preload("res://Pot.tscn")
@@ -28,6 +30,8 @@ var current_evil_time
 var rand_evil_offset
 
 var Items
+
+var cursor_change = false
 
 var level = 0 #How hard the game is
 
@@ -48,9 +52,15 @@ func _process(delta):
 	
 	# Change the cursor image according to the mode we are in. 
 	if(current_mode == MODES.water_mode):
-		Input.set_custom_mouse_cursor(water_cursor)
+		if cursor_change:
+			Input.set_custom_mouse_cursor(water_cursor_clicked)
+		else:
+			Input.set_custom_mouse_cursor(water_cursor)
 	elif(current_mode == MODES.fertilizer_mode):
-		Input.set_custom_mouse_cursor(fertilizer_cursor)
+		if cursor_change:
+			Input.set_custom_mouse_cursor(fertilizer_cursor_clicked)
+		else:
+			Input.set_custom_mouse_cursor(fertilizer_cursor)
 	elif(current_mode == MODES.hand_mode):
 		Input.set_custom_mouse_cursor(hand_cursor)
 	
@@ -66,6 +76,12 @@ func _process(delta):
 	# Change back to hand mode when right click is pressed. 
 	if Input.is_action_pressed("hand_mode") and current_mode != MODES.hand_mode:
 		current_mode = MODES.hand_mode
+		
+	if Input.is_action_just_released("left_click"):
+		if not Input.set_custom_mouse_cursor(hand_cursor):
+			cursor_change = true
+			$CursorChange.start()
+		pass
 	pass
 	
 func find_evil(): #Finding if there are Evil Cacti
@@ -106,4 +122,9 @@ func add_pot():
 func _on_CactiCreator_timeout():
 	print("Adding pot")
 	add_pot()
+	pass # Replace with function body.
+
+
+func _on_CursorChange_timeout():
+	cursor_change = false
 	pass # Replace with function body.
